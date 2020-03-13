f39c4ba06b0ff80366fdfb0102851200a9a945e0
Viktar.Zanimonets <vzanimonets@gmail.com>
2020-03-13 10:21:53 +0300

    refactor: moves icons to individual components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import IconSortAsc from "../Icons/IconSortAsc";
+import IconSortDesc from "../Icons/IconSortDesc";
+
 const Header = ({ column, fnClick, sort }) => {
   const style =
     column.sortKey === "id" || column.sortKey === "age"
@@ -9,38 +12,18 @@ const Header = ({ column, fnClick, sort }) => {
       <span>{column.name}</span>
       <span className="table-column-sorter">
         <span onClick={() => fnClick({ sortKey: column.sortKey, dir: "asc" })}>
-          <svg
-            viewBox="0 0 1024 1024"
-            focusable="false"
-            data-icon="caret-up"
-            width="1em"
-            height="1em"
-            fill={
-              sort.sortKey === column.sortKey && sort.dir === "asc"
-                ? "currentColor"
-                : "#C0C0C0"
-            }
-            aria-hidden="true"
-          >
-            <path d="M858.9 689L530.5 308.2c-9.4-10.9-27.5-10.9-37 0L165.1 689c-12.2 14.2-1.2 35 18.5 35h656.8c19.7 0 30.7-20.8 18.5-35z"></path>
-          </svg>
+          <IconSortAsc
+            columnSort={column.sortKey}
+            currentSort={sort.sortKey}
+            currentDir={sort.dir}
+          />
         </span>
         <span onClick={() => fnClick({ sortKey: column.sortKey, dir: "desc" })}>
-          <svg
-            viewBox="0 0 1024 1024"
-            focusable="false"
-            data-icon="caret-down"
-            width="1em"
-            height="1em"
-            fill={
-              sort.sortKey === column.sortKey && sort.dir === "desc"
-                ? "currentColor"
-                : "#C0C0C0"
-            }
-            aria-hidden="true"
-          >
-            <path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path>
-          </svg>
+          <IconSortDesc
+            columnSort={column.sortKey}
+            currentSort={sort.sortKey}
+            currentDir={sort.dir}
+          />
         </span>
       </span>
     </div>
