64a047235461643e9d0a12ad32809ed157eb3015
vzanimonets <47920165+vzanimonets@users.noreply.github.com>
2020-03-15 20:26:31 +0300

    gh-pages

---
+++ precache-manifest.a27b077870b3eaf370f97d451027fe00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc58fdad9c4c0ce5a3e209954af2a7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf2766f85c84ad2e6d3",
+    "url": "/static/css/main.301a3d6d.chunk.css"
+  },
+  {
+    "revision": "631870999f94c86a6377",
+    "url": "/static/js/2.7adbba7f.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/static/js/2.7adbba7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf2766f85c84ad2e6d3",
+    "url": "/static/js/main.9af37765.chunk.js"
+  },
+  {
+    "revision": "707d41e56b4e984fb3b1",
+    "url": "/static/js/runtime-main.335ad2b1.js"
+  }
+]);
